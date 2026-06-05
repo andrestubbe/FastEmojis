21ae5abb9fa79eef0d904daad2c013164cfe0dca
Andre Stubbe <andrestubbe@proton.me>
2026-06-05 13:58:59 +0200

    Auto Update

--- run-demo.bat
+++ run-demo.bat
@@ -1,11 +1,8 @@
-﻿@echo off
-chcp 65001 >nul
-
+@echo off
 
 echo [FastEmojis] Running Demo...
 cd examples
-cls
-call mvn -q clean compile exec:java -Dexec.mainClass="fastemojis.Demo"
+call mvn clean compile exec:java -Dexec.mainClass="fastemojis.Demo"
 if %ERRORLEVEL% NEQ 0 ( pause & exit /b )
 cd ..
 pause
